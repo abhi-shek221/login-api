d7b2495b2e84553975c5796d8f80c9394910e870
Abhishek CHilakalapalli <abhichilakalapalli@gmail.com>
2024-06-10 19:17:48 +0530

    changed welcome

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./key1.json');
+const serviceAccount = require('./key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser');
 const { admin, db } = require('./firebase'); 
 const path = require('path');
 const passwordHash = require('password-hash');
+const request = require('request');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -18,10 +19,6 @@ app.get('/', (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.render('signup');
-});
-
-app.get('/welcome', (req, res) => {
-    res.render('welcome');
 });
 
 app.post('/login', async (req, res) => {
@@ -36,7 +33,7 @@ app.post('/login', async (req, res) => {
         if (!passwordHash.verify(password, userData.password)) {
             return res.send('Incorrect password. Please try again.');
         }
-        res.render('welcome', { username: userData.userName });
+        res.render('welcome', { username: userData.userName, celebrity: null, error: null });
     } catch (error) {
         res.send('Error logging in');
     }
@@ -64,6 +61,38 @@ app.post('/signupSubmit', async (req, res) => {
     } catch (error) {
         res.send('Something went wrong...');
     }
+});
+
+app.post('/getCelebrity', (req, res) => {
+    const { celebrity } = req.body;
+
+    request.get({
+        url: `https://api.api-ninjas.com/v1/celebrity?name=${encodeURIComponent(celebrity)}`,
+        headers: {
+            'X-Api-Key': 'l4eaoiHBZHClB2QSKa3Taw==MtPh11YAAwkJbOVL'
+        },
+    }, function (error, response, body) {
+        if (error) {
+            return res.render('welcome', { username: req.body.username, error: 'Request failed: ' + error, celebrity: null });
+        } else if (response.statusCode !== 200) {
+            return res.render('welcome', { username: req.body.username, error: 'Error: ' + response.statusCode, celebrity: null });
+        } else {
+            try {
+                const obj = JSON.parse(body);
+                if (obj && obj[0]) {
+                    return res.render('welcome', {
+                        username: req.body.username,
+                        celebrity: obj[0],
+                        error: null
+                    });
+                } else {
+                    return res.render('welcome', { username: req.body.username, error: 'No data found', celebrity: null });
+                }
+            } catch (parseError) {
+                return res.render('welcome', { username: req.body.username, error: 'Error parsing JSON response: ' + parseError.message, celebrity: null });
+            }
+        }
+    });
 });
 
 app.listen(port, () => {
